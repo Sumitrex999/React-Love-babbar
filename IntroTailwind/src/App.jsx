09ca41ad0964fd9cc4f7b358ed0ec9da2c7bbde0
Sumit Rex <sumitrex666@gmail.com>
2025-12-03 10:58:25 +0530

    React Intro Tailwiind

---
+++ IntroTailwind/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <nav>
+        <ul class="py-8 bg-purple-400 flex">
+          <li class='mx-4 text-white'>Home</li>
+          <li class='mx-4 text-white'>Projects</li>
+          <li class='mx-4 text-white'>Contact Us</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default App
